650129fcc6655a11b6e19919abcd1c85357196cb
leonp <leonp@DESKTOP-2032062>
2022-11-04 09:44:06 +0100

    Add test anyone should be able to deploy

---
+++ contracts/contracts/Escrow.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Escrow is Ownable {
+
+    constructor ()  {
+        
+    }
+}
