db4a4f4286d25e05d56c80c4e40842f2aac0621b
Leon de Pruyssenaere de la Woestijne <git@wieisleon.nl>
2022-11-07 21:19:51 +0100

    Test name for hackatonmanager

--- contracts/tests/HackathonManagerFactory.ts
+++ contracts/tests/HackathonManagerFactory.ts
@@ -2,6 +2,7 @@ import { Signer } from "@ethersproject/abstract-signer";
 import { Wallet } from "@ethersproject/wallet";
 import { expect } from "chai";
 import { ethers, network } from "hardhat";
+import { HackathonManager } from "../typechain-types";
 
 describe("HackathonManagerFactory", function () {
 
@@ -49,4 +50,36 @@ describe("HackathonManagerFactory", function () {
         } );
     });
 
+});
+
+describe("HackatonManager", function () {
+
+    let hackatonManagerAddress : string;
+    const hackName = "hackatonmanager";
+
+    beforeEach(async function () {
+        
+            
+            const HackathonManagerFactory = await ethers.getContractFactory("HackathonManagerFactory");
+            const hackathonManagerFactory = await HackathonManagerFactory.deploy();
+            
+            const rc = await (await hackathonManagerFactory.createNewHack(hackName,{value: 1})).wait();
+            
+            const event = rc.events?.find(event => event.event === 'HackCreated');
+            hackatonManagerAddress = event?.args?._contractAddress ?? '';
+            
+      });
+
+      it('hackatonmanager has a name', async function() {
+
+        const hackatonmanager =(await ethers.getContractFactory("HackathonManager"))
+        .attach(hackatonManagerAddress);
+        
+        
+        console.log('THe name was: ' + await hackatonmanager._hackathonName());
+        await expect(await hackatonmanager._hackathonName())
+            .to.equal(hackName);
+
+        
+      });
 });
