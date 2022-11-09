3b1c580b5bb41dd52db501dfe1bc11fddf08538b
eldnctlh <cequeditlabouchedombre@gmail.com>
2022-11-10 01:04:33 +0200

    funds display

--- dapp/src/components/HackatonDetails/HackatonDetails.tsx
+++ dapp/src/components/HackatonDetails/HackatonDetails.tsx
@@ -5,6 +5,7 @@ import HackatonExtraData from "components/forms/HackatonExtraData"
 import FundHackaton from "components/forms/FundHackaton"
 import useHackatonManager from "utils/context/hackatonManagerContext"
 import Loader from "components/shared/Loader"
+import { ethers } from "ethers"
 
 const HackatonDetails = () => {
     const { initHackatonManager, hackatonState, loading } = useHackatonManager()
@@ -15,13 +16,17 @@ const HackatonDetails = () => {
             initHackatonManager(query.address)
         }
     }, [query.address])
-
+    console.log(hackatonState)
     const renderForms = () => {
         if (hackatonState.funded) {
             return (
                 <div className="grid grid-cols-5 gap-4">
                     <div className="col-span-3 bg-zinc-800 px-10 py-10">
                         <h2 className="text-4xl my-5 font-bold text-gray-100">Manage Hackaton</h2>
+                        <p className="mb-4 text-lg text-gray-400">
+                            Total amount of funds: {ethers.utils.formatEther(hackatonState.funds)}{" "}
+                            ETH
+                        </p>
                         <HackatonMainData />
                     </div>
                     <div className="col-span-2 py-10">
