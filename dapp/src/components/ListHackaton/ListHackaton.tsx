62ed28c393083106a5c6beb83078df3c0791e8f5
eldnctlh <cequeditlabouchedombre@gmail.com>
2022-11-13 23:29:13 +0200

    added loader to hack list

--- dapp/src/components/ListHackaton/ListHackaton.tsx
+++ dapp/src/components/ListHackaton/ListHackaton.tsx
@@ -1,41 +1,46 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import useHackatonManagerFactory from "utils/context/hackatonManagerFactoryContext"
 
 import Link from "next/link"
+import Loader from "components/shared/Loader"
 
 const ListHackaton = () => {
-    const {listOfHackatons} = useHackatonManagerFactory();
-    
-return (
+    const { listOfHackatons, initHackatonManagerFactory, loading } = useHackatonManagerFactory()
+
+    useEffect(() => {
+        initHackatonManagerFactory()
+    }, [])
+
+    return (
         <div className="container mx-auto py-10">
             <h2 className="text-4xl my-5 font-bold text-gray-100">List of Hackatons</h2>
-            <p className="mb-4 text-lg text-gray-400">
-                
-            </p>
-            <div className="grid grid-cols-5 gap-4">
-                <div className="col-span-2">
-                    
-                    {listOfHackatons?.map((item) => 
-                        <div>
-                            <li>{item[0]}
-                              <Link href={`/manage-hackaton/${item[1]}`}>
-                                <p className="cursor-pointer text-blue-300">
-                                    Manage hackaton
-                                </p>
-                            </Link>
-                            <Link href={`/hackaton/${item[1]}`}>
-                                <p className="cursor-pointer text-blue-300">
-                                    Join hackaton
-                                </p>
-                            </Link>
-                            </li>
-                            </div>
-                        
-                        
-                    )}
+            {loading ? (
+                <div className="flex justify-center">
+                    <Loader />
                 </div>
-
-            </div>
+            ) : (
+                <div className="grid grid-cols-5 gap-4">
+                    <div className="col-span-2">
+                        {listOfHackatons?.map((item) => (
+                            <div>
+                                <li>
+                                    {item[0]}
+                                    <Link href={`/manage-hackaton/${item[1]}`}>
+                                        <p className="cursor-pointer text-blue-300">
+                                            Manage hackaton
+                                        </p>
+                                    </Link>
+                                    <Link href={`/hackaton/${item[1]}`}>
+                                        <p className="cursor-pointer text-blue-300">
+                                            Join hackaton
+                                        </p>
+                                    </Link>
+                                </li>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
