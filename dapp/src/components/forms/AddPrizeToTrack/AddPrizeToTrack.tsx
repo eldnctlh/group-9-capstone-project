1ca2003200be080f15db32d62daf0479d75c1457
eldnctlh <cequeditlabouchedombre@gmail.com>
2022-11-14 18:23:10 +0200

    code refactoring

--- dapp/src/components/forms/AddPrizeToTrack/AddPrizeToTrack.tsx
+++ dapp/src/components/forms/AddPrizeToTrack/AddPrizeToTrack.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
-import { useFieldArray, useForm } from "react-hook-form"
-import { useRouter } from "next/router"
+import { useForm } from "react-hook-form"
 import { toast } from "react-toastify"
 import Input from "components/shared/Input"
 import Button from "components/shared/Button"
@@ -11,9 +10,8 @@ import { extractRevertReason } from "utils/helpers"
 
 const AddPrizeToTrack: React.FC = () => {
     const [loading, setLoading] = useState<boolean>(false)
-    const { addCID, hackatonState, addPrizeToTrack } = useHackatonManager()
+    const { hackatonState, addPrizeToTrack } = useHackatonManager()
 
-    const { query } = useRouter()
     const { connected } = useWallet()
 
     const {
@@ -22,128 +20,87 @@ const AddPrizeToTrack: React.FC = () => {
         formState: { errors },
     } = useForm()
 
-    const onSubmit = async data => { 
-        console.log(data);
-    
+    const onSubmit = async (data) => {
         setLoading(true)
-
-        const addPrice : PriceForTrack =
-        {
+        const addPrice: PriceForTrack = {
             trackName: data.trackName,
             prizeAmount: data.prizeAmount,
-            prizeName : data.prizeName
+            prizeName: data.prizeName,
         }
-            
-            try {
-                await  addPrizeToTrack(addPrice)
-                
-                toast(`${addPrice.prizeName} added to track ${addPrice.trackName} for a whopping: ${addPrice.prizeAmount} ETH`)
-            } catch (err) {
-                const msg = extractRevertReason(err)
 
-                console.log(err)
-                toast.error(msg || err.message)
-            }
-        
+        try {
+            await addPrizeToTrack(addPrice)
+
+            toast(
+                `${addPrice.prizeName} added to track ${addPrice.trackName} for a whopping: ${addPrice.prizeAmount} ETH`
+            )
+        } catch (err) {
+            const msg = extractRevertReason(err)
+
+            console.log(err)
+            toast.error(msg || err.message)
+        }
+
         setLoading(false)
     }
     const renderTracks = () =>
-    hackatonState.tracks.length ? (
-        <div>
-            <div className="flex items-center mt-4">
-                <h2 className="text-xl font-bold text-gray-200 whitespace-nowrap">
-                    Add prize
-                </h2>
-                <div className="ml-5 h-px w-full bg-zinc-400"></div>
-            </div>
-            <div className="grid grid-cols-1 gap-4">
-                {hackatonState.tracks.map((track) => (
-                    
-                    <div key={track.name} className="col-span-2 mt-2">
-                        <h3 className="mt-1 font-bold text-gray-200">{track.name}</h3>
-                        <p className="mt-1 text-gray-400">
-                            {ethers.utils.formatEther(track.poolAmount)} ETH
-                        </p>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-         
-                <div  className="flex justify-between items-end">
-                    <div className="flex">
-                        <input {...register("trackName")} value ={track.name} type="hidden" />
-                        <Input
-                            placeholder={`Prize Name`}
-                            label={`Prize Name`}
-                            error={
-                                errors.prizeName &&
-                                "Prize name is required."
-                            }
-                            {...register(`prizeName`, { required: true })}
-                        />
-                        <Input
-                            placeholder={`Prize`}
-                            label={`Prize (ETH)`}
-                            error={
-                                errors.prizeAmount &&
-                                "Prize is required."
-                            }
-                            className="ml-3"
-                            {...register(`prizeAmount`, { required: true })}
-                        />
-                    </div>
- 
-                </div>
-            
+        hackatonState.tracks.length ? (
+            <div>
+                <div className="grid grid-cols-1 gap-4">
+                    {hackatonState.tracks.map((track) => (
+                        <div key={track.name} className="col-span-2 mt-2">
+                            <h3 className="mt-1 font-bold text-gray-200">{track.name}</h3>
+                            <p className="mt-1 text-gray-400">
+                                {ethers.utils.formatEther(track.poolAmount)} ETH
+                            </p>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className="flex justify-between items-end">
+                                    <div className="flex">
+                                        <input
+                                            {...register("trackName")}
+                                            value={track.name}
+                                            type="hidden"
+                                        />
+                                        <Input
+                                            placeholder={`Prize Name`}
+                                            label={`Prize Name`}
+                                            error={errors.prizeName && "Prize name is required."}
+                                            {...register(`prizeName`, { required: true })}
+                                        />
+                                        <Input
+                                            placeholder={`Prize`}
+                                            label={`Prize (ETH)`}
+                                            error={errors.prizeAmount && "Prize is required."}
+                                            className="ml-3"
+                                            {...register(`prizeAmount`, { required: true })}
+                                        />
+                                    </div>
+                                </div>
 
-            <div className="flex justify-between">
-                <Button
-                    loading={loading}
-                    disabled={!connected || loading}
-                    className="mt-5"
-                    onClick={() => handleSubmit(onSubmit)}
-                >
-                    Add Prize
-                </Button>
+                                <div className="flex justify-between">
+                                    <Button
+                                        loading={loading}
+                                        disabled={!connected || loading}
+                                        className="mt-5"
+                                        onClick={() => handleSubmit(onSubmit)}
+                                    >
+                                        Add Prize
+                                    </Button>
+                                </div>
+                            </form>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </form>
-                    </div>
-                    
-                ))}
-            </div>
-            <div className="mt-4 mb-10 h-px w-full bg-zinc-400"></div>
-        </div>
-    ) : null
+        ) : null
 
     const renderForms = () => {
-        
         if (hackatonState.funded) {
-            return (
-                <div className="grid grid-cols-5 gap-4">
-                    <div className="col-span-3 bg-zinc-800 px-10 py-10">
-                        <p className="mb-2 text-lg text-gray-400">
-                            Total amount of funds: {ethers.utils.formatEther(hackatonState.funds)}{" "}
-                            ETH
-                        </p>
-
-                        {renderTracks()}                       
-                    </div>                  
-                </div>            
-            )
-        } else {
-            return (
-                <div className="grid grid-cols-5 gap-4">
-    
-                </div>
-            )
+            return <div className="col-span-3 bg-zinc-800 py-5">{renderTracks()}</div>
         }
+        return null
     }
 
-    
-    return (
-       
-      
-        <div className="container mx-auto py-10">
-            {renderForms()}
-            
-            </div>
-    )
+    return <div className="container mx-auto">{renderForms()}</div>
 }
 export default AddPrizeToTrack
